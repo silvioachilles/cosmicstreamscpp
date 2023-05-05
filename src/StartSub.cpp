1755cc4b6626de51296cd6f267e42b69d89407ee
Silvio Achilles <silvio.achilles@desy.de>
2023-05-04 18:58:34 -0700

    Changed StartSub to use highjson instead of JsonCpp

--- src/StartSub.cpp
+++ src/StartSub.cpp
@@ -1,4 +1,5 @@
 #include "StartSub.h"
+#include "highjson.h"
 
 using std::unique_ptr;
 
@@ -11,32 +12,12 @@ StartSub::StartSub(const string& host, const int& port, const string& topic):
 Json::Value StartSub::recv_start() {
     vector<void*> datas;
     vector<size_t> sizes;
+
     recv_multipart(datas, sizes);
 
     void* metadata_void = datas[1];
     size_t size = sizes[1];
 
-    string metadata_str = string(static_cast<const char*>(metadata_void), size);
-
-    Json::CharReaderBuilder builder;
-    const unique_ptr<Json::CharReader> reader(builder.newCharReader());
-
-    Json::Value metadata;
-    JSONCPP_STRING err;
-
-    bool read_success = reader->parse(
-            metadata_str.c_str(),
-            metadata_str.c_str() + metadata_str.length(),
-            &metadata,
-            &err
-            );
-
-    if (read_success)
-    {
-        return metadata;
-    }
-    else
-    {
-        throw std::logic_error("Could not parse received metadata.");
-    }
+    Json::Value metadata = highjson::loads(metadata_void, size);
+    return metadata;
 }
