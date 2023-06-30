2f20044c8b96c194bb30b7c00f6013b45f764b7b
Silvio Achilles <silvio.achilles@desy.de>
2023-06-30 15:34:41 -0700

    W&P commit before leaving lbnl

--- include/cosmicstreamscpp/RecoStream.h
+++ include/cosmicstreamscpp/RecoStream.h
@@ -1,41 +1,17 @@
-#include "cosmicstreamscpp/Sub.h"
-#include "cosmicstreamscpp/Pub.h"
-#include "cosmicstreamscpp/defaults.h"
+#pragma once
 
+#include <zmq.hpp>
 #include <string>
+#include <json/json.h>
+#include <cosmicstreamscpp/structs.h>
 
 using std::string;
 
+class Sub;
+class Pub;
+
 class RecoStream {
 public:
-    Sub m_socket_start;
-    string m_start_host;
-    int m_start_port = START_PORT;
-    string m_start_topic = START_TOPIC;
-
-    Sub m_socket_frame;
-    string m_frame_host;
-    int m_frame_port = FRAME_PORT;
-    string m_frame_topic = FRAME_TOPIC;
-
-    Sub m_socket_stop;
-    string m_stop_host;
-    int m_stop_port = STOP_PORT;
-    string m_stop_topic = STOP_TOPIC;
-
-    Sub m_socket_abort;
-    string m_abort_host;
-    int m_abort_port = ABORT_PORT;
-    string m_abort_topic = ABORT_TOPIC;
-
-    Pub m_socket_rec;
-    string m_rec_host = PUB_HOST_ALL;
-    int m_rec_port = REC_PORT;
-    string m_rec_topic = REC_TOPIC;
-    bool m_use_out = true;
-
-    zmq::poller_t<> m_poller;
-
     RecoStream() = default;
     RecoStream(const string& host_start,
                const string& host_frame,
@@ -43,8 +19,35 @@ public:
                const string& host_abort,
                const bool& use_out);
 
+    Sub* m_socket_start;
+    string m_start_host;
+    int m_start_port;
+    string m_start_topic;
+
+    Sub* m_socket_frame;
+    string m_frame_host;
+    int m_frame_port;
+    string m_frame_topic;
+
+    Sub* m_socket_stop;
+    string m_stop_host;
+    int m_stop_port;
+    string m_stop_topic;
+
+    Sub* m_socket_abort;
+    string m_abort_host;
+    int m_abort_port;
+    string m_abort_topic;
+
+    Pub* m_socket_rec;
+    string m_rec_host;
+    int m_rec_port;
+    string m_rec_topic;
+    bool m_use_out = true;
+
     void init_sockets();
 
+    zmq::poller_t<> m_poller;
     zmq::poller_event<> get_event();
 
     bool something_in_queue();
--- src/RecoStream.cpp
+++ src/RecoStream.cpp
@@ -1,4 +1,9 @@
-#include "include/cosmicstreamscpp/RecoStream.h"
+#include "cosmicstreamscpp/RecoStream.h"
+#include "cosmicstreamscpp/Sub.h"
+#include "cosmicstreamscpp/Pub.h"
+#include "cosmicstreamscpp/defaults.h"
+#include "cosmicstreamscpp/structs.h"
+#include <json/json.h>
 
 #include <vector>
 #include <chrono>
@@ -17,22 +22,38 @@ RecoStream::RecoStream(const string& host_start,
     m_abort_host = host_abort;
 
     m_use_out = use_out;
+
+    m_start_port = START_PORT;
+    m_start_topic = START_TOPIC;
+
+    m_frame_port = FRAME_PORT;
+    m_frame_topic = FRAME_TOPIC;
+
+    m_stop_port = STOP_PORT;
+    m_stop_topic = STOP_TOPIC;
+
+    m_abort_port = ABORT_PORT;
+    m_abort_topic = ABORT_TOPIC;
+
+    m_rec_host = PUB_HOST_ALL;
+    m_rec_port = REC_PORT;
+    m_rec_topic = REC_TOPIC;
 }
 
 void RecoStream::init_sockets() {
-    m_socket_start = Sub(m_start_host, m_start_port, m_start_topic);
-    m_socket_frame = Sub(m_frame_host, m_frame_port, m_frame_topic);
-    m_socket_stop = Sub(m_stop_host, m_stop_port, m_stop_topic);
-    m_socket_abort = Sub(m_abort_host, m_abort_port, m_abort_topic);
+    m_socket_start = new Sub(m_start_host, m_start_port, m_start_topic);
+    m_socket_frame = new Sub(m_frame_host, m_frame_port, m_frame_topic);
+    m_socket_stop = new Sub(m_stop_host, m_stop_port, m_stop_topic);
+    m_socket_abort = new Sub(m_abort_host, m_abort_port, m_abort_topic);
     if (m_use_out) {
-        m_socket_rec = Pub(m_rec_host, m_rec_port, m_rec_topic);
+        m_socket_rec = new Pub(m_rec_host, m_rec_port, m_rec_topic);
     }
 
     m_poller = zmq::poller_t<> ();
-    m_poller.add(m_socket_start.m_socket_ref, zmq::event_flags::pollin);
-    m_poller.add(m_socket_frame.m_socket_ref, zmq::event_flags::pollin);
-    m_poller.add(m_socket_stop.m_socket_ref, zmq::event_flags::pollin);
-    m_poller.add(m_socket_abort.m_socket_ref, zmq::event_flags::pollin);
+    m_poller.add(m_socket_start->m_socket_ref, zmq::event_flags::pollin);
+    m_poller.add(m_socket_frame->m_socket_ref, zmq::event_flags::pollin);
+    m_poller.add(m_socket_stop->m_socket_ref, zmq::event_flags::pollin);
+    m_poller.add(m_socket_abort->m_socket_ref, zmq::event_flags::pollin);
 }
 
 zmq::poller_event<> RecoStream::get_event() {
@@ -55,7 +76,7 @@ bool RecoStream::something_in_queue() {
 bool RecoStream::has_start_arrived() {
     zmq::poller_event<> event = get_event();
 
-    if (event.socket.handle() == m_socket_start.m_socket.handle()) {
+    if (event.socket.handle() == m_socket_start->m_socket.handle()) {
         return true;
     }
 
@@ -65,7 +86,7 @@ bool RecoStream::has_start_arrived() {
 bool RecoStream::has_frame_arrived() {
     zmq::poller_event<> event = get_event();
 
-    if (event.socket.handle() == m_socket_frame.m_socket.handle()) {
+    if (event.socket.handle() == m_socket_frame->m_socket.handle()) {
         return true;
     }
 
@@ -75,7 +96,7 @@ bool RecoStream::has_frame_arrived() {
 bool RecoStream::has_stop_arrived() {
     zmq::poller_event<> event = get_event();
 
-    if (event.socket.handle() == m_socket_stop.m_socket.handle()) {
+    if (event.socket.handle() == m_socket_stop->m_socket.handle()) {
         return true;
     }
 
@@ -85,7 +106,7 @@ bool RecoStream::has_stop_arrived() {
 bool RecoStream::has_abort_arrived() {
     zmq::poller_event<> event = get_event();
 
-    if (event.socket.handle() == m_socket_abort.m_socket.handle()) {
+    if (event.socket.handle() == m_socket_abort->m_socket.handle()) {
         return true;
     }
 
@@ -93,23 +114,23 @@ bool RecoStream::has_abort_arrived() {
 }
 
 Json::Value RecoStream::recv_start() {
-    return m_socket_start.recv_json();
+    return m_socket_start->recv_json();
 }
 
 Frame RecoStream::recv_frame() {
-    return m_socket_frame.recv_frame();
+    return m_socket_frame->recv_frame();
 }
 
 Json::Value RecoStream::recv_stop() {
-    return m_socket_stop.recv_json();
+    return m_socket_stop->recv_json();
 }
 
 Json::Value RecoStream::recv_abort() {
-    return m_socket_abort.recv_json();
+    return m_socket_abort->recv_json();
 }
 
 void RecoStream::send_rec(const Reco& reco) {
     if (m_use_out) {
-        m_socket_rec.send_rec(reco);
+        m_socket_rec->send_rec(reco);
     }
 }
